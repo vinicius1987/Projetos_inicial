867834f89d29f773a861d2985d151f2d5c9ec251
vinicius <vinicius@10.0.0.104>
2022-05-30 23:59:23 -0300

    Iniciando validações e alterando view

--- eventoapp/src/main/java/com/eventoapp/controller/EventoController.java
+++ eventoapp/src/main/java/com/eventoapp/controller/EventoController.java
@@ -2,10 +2,13 @@ package com.eventoapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.eventoapp.models.Convidado;
 import com.eventoapp.models.Evento;
@@ -47,14 +50,23 @@ public class EventoController {
 		Evento evento = er.findByCodigo(codigo);
 		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
 		mv.addObject("evento", evento);
+		
+		Iterable<Convidado> convidado = cr.findByEvento(evento);
+		mv.addObject("convidados", convidado);
+		
 		return mv;
 	}
 	
 	@RequestMapping(value = "/{codigo}", method = RequestMethod.POST)
-	public String detalhesEventoPost(@PathVariable("codigo") long codigo, Convidado convidado) {
+	public String detalhesEventoPost(@PathVariable("codigo") long codigo, @Validated Convidado convidado, BindingResult result, RedirectAttributes atributes) {
+		if(result.hasErrors()) {
+			atributes.addFlashAttribute("mensagem", "Verifique os Campos!");
+			return "redirect:/{codigo}";
+		}
 		Evento evento = er.findByCodigo(codigo);
 		convidado.setEvento(evento);
 		cr.save(convidado);
+		atributes.addFlashAttribute("mensagem", "Convidado adicionado com sucesso");
 		return "redirect:/{codigo}";
 	}
 	
--- eventoapp/src/main/java/com/eventoapp/models/Convidado.java
+++ eventoapp/src/main/java/com/eventoapp/models/Convidado.java
@@ -1,14 +1,19 @@
 package com.eventoapp.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+
 
 @Entity
 public class Convidado {
 	
 	@Id
+	@Column(nullable = false)
 	private String rg;
+	
+	@Column(nullable = false)
 	private String nomeConvidado;
 	
 	@ManyToOne
